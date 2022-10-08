a4e19288427c7316e7e5bf64848a56195271b297
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-10-08 16:42:17 -0300

    Test (LocalFeedLoader): Garantee `save` method calls deletion operation from store colaborator

--- EssentialFeed/EssentialFeedTests/LocalFeed/CacheFeedUseCaseTest.swift
+++ EssentialFeed/EssentialFeedTests/LocalFeed/CacheFeedUseCaseTest.swift
@@ -1,10 +1,15 @@
 import XCTest
+import EssentialFeed
 
 final class LocalFeedLoader {
     let store: FeedStore
     
     init(store: FeedStore) {
         self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCache()
     }
 }
 
@@ -15,8 +20,37 @@ final class CacheFeedUseCaseTest: XCTestCase {
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        let items = uniqueItem().asList
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+}
+
+private extension CacheFeedUseCaseTest {
+    func uniqueItem() -> FeedItem {
+        .init(
+            id: .init(), description: "any-description",
+            location: "any-location", imageURL: URL(string: "https://any-url.com")!
+        )
+    }
+}
+
+extension FeedItem {
+    var asList: [FeedItem] {
+        [self]
+    }
 }
 
 final class FeedStore {
     private(set) var deleteCachedFeedCallCount = 0
+    
+    func deleteCache() {
+        deleteCachedFeedCallCount += 1
+    }
 }
