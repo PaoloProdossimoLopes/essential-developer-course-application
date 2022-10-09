eb76a53905cd7f55c30acf78ad702944b59b67e0
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-10-09 12:00:25 -0300

    Test (LoadFeedFromCacheUseCase): Does not message store upon creation

--- EssentialFeed/EssentialFeedTests/LocalFeed/CacheFeedUseCaseTest.swift
+++ EssentialFeed/EssentialFeedTests/LocalFeed/CacheFeedUseCaseTest.swift
@@ -143,46 +143,6 @@ private extension CacheFeedUseCaseTest {
         let lcoal = model.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image) }
         return (model, lcoal)
     }
-    
-    final class FeedStoreSpy: FeedStore {
-        
-        enum RecievedMessages: Equatable {
-            case deleteCache
-            case insert([LocalFeedImage], Date)
-        }
-        
-        private(set) var recievedMessages = [RecievedMessages]()
-        private var deletionsCompletion = [DeletionCompletion]()
-        private var insertionCompletions = [InsertionCompletion]()
-        
-        //MARK: - Methods
-        func deleteCache(completion: @escaping DeletionCompletion) {
-            deletionsCompletion.append(completion)
-            recievedMessages.append(.deleteCache)
-        }
-        
-        func insertCache(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-            recievedMessages.append(.insert(items, timestamp))
-            insertionCompletions.append(completion)
-        }
-        
-        //MARK: - Spies
-        func completeDeletion(with error: Error?, at index: Int = 0) {
-            deletionsCompletion[index](error)
-        }
-        
-        func completeDeletionSuccessfull(at index: Int = 0) {
-            deletionsCompletion[index](nil)
-        }
-        
-        func completeInsertion(with error: Error?, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
-        
-        func completeInsertionSuccessfull(at index: Int = 0) {
-            insertionCompletions[index](nil)
-        }
-    }
 }
 
 extension FeedImage {
---
+++ EssentialFeed/EssentialFeedTests/LocalFeed/LoadFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,30 @@
+import XCTest
+import EssentialFeed
+
+final class LoadFeedFromCacheUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotMessageCache_uponSUTCreation() {
+        let (_, store) = makeSUT()
+        XCTAssertEqual(store.recievedMessages, [])
+    }
+}
+
+//MARK: - Heleper
+private extension LoadFeedFromCacheUseCaseTests {
+    func makeSUT(
+        currentDate: @escaping (() -> Date) = Date.init,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (
+        sut: LocalFeedLoader,
+        store: FeedStoreSpy
+    ) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        
+        checkMemoryLeak(sut, file: file, line: line)
+        checkMemoryLeak(store, file: file, line: line)
+        
+        return (sut, store)
+    }
+}
