76f0a3ad8d9db14c3191df360edd35c0df278c5e
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-10-16 20:32:00 -0300

    Refactor (FeedCachePolicy): Move It's own file

--- EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -1,18 +1,5 @@
 import Foundation
 
-private enum FeedCachePolicy {
-    
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        let calendar = Calendar(identifier: .gregorian)
-        let maxExpiredDays = 7
-        if let maxRangeCached = calendar.date(byAdding: .day, value: maxExpiredDays, to: timestamp) {
-            return date < maxRangeCached
-        }
-        
-        return false
-    }
-}
-
 public final class LocalFeedLoader {
     
     let store: FeedStore
