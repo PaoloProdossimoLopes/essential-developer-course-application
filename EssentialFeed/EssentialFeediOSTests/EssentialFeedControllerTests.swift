06bfdcd5b9f9524c354046eb1d563d93739ebe4f
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-11-03 08:31:37 -0300

    feat: enable pull to refresh for loader in table view

--- EssentialFeed/EssentialFeediOSTests/EssentialFeedControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/EssentialFeedControllerTests.swift
@@ -3,7 +3,7 @@ import EssentialFeed
 //@testable import EssentialFeediOS
 
 import UIKit
-final class EssentialFeedController: UIViewController {
+final class EssentialFeedController: UITableViewController {
     
     private var loader: IFeedLoader?
     
@@ -14,6 +14,19 @@ final class EssentialFeedController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        refreshControl = .init()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        
+        loadFeed()
+    }
+    
+    @objc private func load() {
+        loadFeed()
+    }
+    
+    //MARK: - Helpers
+    private func loadFeed() {
         loader?.load { _ in }
     }
 }
@@ -30,16 +43,34 @@ final class EssentialFeedControllerTests: XCTestCase {
         sut.loadViewIfNeeded()
         XCTAssertEqual(loader.loadCallCount, 1)
     }
+    
+    func test_pullToRefresh_loadsFeed() {
+        let (sut, loader) = makeEnviroment()
+        sut.loadViewIfNeeded()
+        
+        sut.refreshControl?.simulatePullToRefresh()
+        
+        XCTAssertEqual(loader.loadCallCount, 2)
+    }
 }
 
+//MARK: - Helpers
 private extension EssentialFeedControllerTests {
-    func makeEnviroment() -> (sut: EssentialFeedController, loader: LoaderSpy) {
+    func makeEnviroment(
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (sut: EssentialFeedController, loader: LoaderSpy) {
         let loader = LoaderSpy()
         let sut = EssentialFeedController(loader: loader)
+        
+        checkMemoryLeak(sut, file: file, line: line)
+        checkMemoryLeak(loader, file: file, line: line)
+        
         return (sut, loader)
     }
 }
 
+//MARK: - Doubles
 private extension EssentialFeedControllerTests {
     
     final class LoaderSpy: IFeedLoader {
@@ -47,6 +78,23 @@ private extension EssentialFeedControllerTests {
         
         func load(completion: @escaping ((FeedResult) -> Void)) {
             loadCallCount += 1
+        }
+    }
+}
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        simulate(.valueChanged)
+    }
+}
+
+private extension UIControl {
+    func simulate(_ event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach {
+                let object = (target as NSObject)
+                object.perform(Selector($0))
+            }
         }
     }
 }
