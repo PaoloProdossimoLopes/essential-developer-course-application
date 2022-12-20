614d11e35c300afcd9249f154862efe38bc3cc9b
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-20 14:01:42 -0300

    feat: link outlets for storyboard (feed)

--- EssentialFeed/EssentialFeediOS/Feed UI/Views/FeedImageCell.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/FeedImageCell.swift
@@ -1,28 +1,16 @@
-//
-//  FeedImageCell.swift
-//  EssentialFeediOS
-//
-//  Created by Paolo Prodossimo Lopes on 09/11/22.
-//
-
 import UIKit
 
-final class FeedImageCell: UITableViewCell {
-    public lazy var imageContainer = UIView()
-    public lazy var feedImageView = UIImageView()
-    public lazy var localtionContainer = UIView()
-    public lazy var descriptionLabel = UILabel()
-    public lazy var localtionLabel = UILabel()
-    
-    public lazy var feedImageRetryButton: UIButton = {
-        let button = UIButton()
-        button.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
-        return button
-    }()
-    
-    @objc private func retryButtonTapped() {
-        onRetry?()
-    }
+public final class FeedImageCell: UITableViewCell {
+    @IBOutlet private(set) public var locationContainer: UIView!
+    @IBOutlet private(set) public var locationLabel: UILabel!
+    @IBOutlet private(set) public var feedImageContainer: UIView!
+    @IBOutlet private(set) public var feedImageView: UIImageView!
+    @IBOutlet private(set) public var feedImageRetryButton: UIButton!
+    @IBOutlet private(set) public var descriptionLabel: UILabel!
     
     var onRetry: (() -> Void)?
+    
+    @IBAction private func retryButtonTapped() {
+        onRetry?()
+    }
 }
