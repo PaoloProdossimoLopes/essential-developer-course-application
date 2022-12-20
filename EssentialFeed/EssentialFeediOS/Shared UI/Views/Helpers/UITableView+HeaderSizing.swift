56e8c3a5b1c5fbca8fa84064ee0c680fbe24820d
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-20 13:54:54 -0300

    refactor: creating a table view header helper

--- EssentialFeed/EssentialFeediOS/Shared UI/Views/Helpers/UITableView+HeaderSizing.swift
+++ EssentialFeed/EssentialFeediOS/Shared UI/Views/Helpers/UITableView+HeaderSizing.swift
@@ -1,8 +1,15 @@
-//
-//  UITableView+HeaderSizing.swift
-//  EssentialFeediOS
-//
-//  Created by Paolo Prodossimo Lopes on 20/12/22.
-//
+import UIKit
 
-import Foundation
+extension UITableView {
+    func sizeTableHeaderToFit() {
+        guard let header = tableHeaderView else { return }
+        
+        let size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        
+        let needsFrameUpdate = header.frame.height != size.height
+        if needsFrameUpdate {
+            header.frame.size.height = size.height
+            tableHeaderView = header
+        }
+    }
+}
