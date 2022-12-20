8ad33da27268cf8d08939d13668776d8f6277545
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-20 13:54:26 -0300

    refactor: creating a container view helper

--- EssentialFeed/EssentialFeediOS/Shared UI/Views/Helpers/UIView+Container.swift
+++ EssentialFeed/EssentialFeediOS/Shared UI/Views/Helpers/UIView+Container.swift
@@ -1,8 +1,22 @@
-//
-//  UIView+Container.swift
-//  EssentialFeediOS
-//
-//  Created by Paolo Prodossimo Lopes on 20/12/22.
-//
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    public func makeContainer() -> UIView {
+        let container = UIView()
+        container.backgroundColor = .clear
+        container.addSubview(self)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            container.trailingAnchor.constraint(equalTo: trailingAnchor),
+            topAnchor.constraint(equalTo: container.topAnchor),
+            container.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+        
+        return container
+    }
+    
+}
