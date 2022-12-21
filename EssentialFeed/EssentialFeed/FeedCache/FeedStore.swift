a3b6b8e62c7485a28e39da8e69bc815a728f227c
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-21 09:26:50 -0300

    refactor: simplify the feed store

--- EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
@@ -1,24 +1,9 @@
-//
-//  FeedStore.swift
-//  EssentialFeed
-//
-//  Created by Paolo Prodossimo Lopes on 08/10/22.
-//
-
 import Foundation
 
-public enum RetrieveFeedCacheResult {
-    case empty
-    case found(feed: [LocalFeedImage], timestamp: Date)
-    case failure(Error)
-}
+public typealias CachedFeed = (feed: [LocalFeedImage], timestamp: Date)
 
 public protocol FeedStore {
-    typealias DeletionCompletion = ((Error?) -> Void)
-    typealias InsertionCompletion = ((Error?) -> Void)
-    typealias RetrieveCompletion = ((RetrieveFeedCacheResult) -> Void)
-    
-    func deleteCache(completion: @escaping DeletionCompletion)
-    func insertCache(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
-    func retrieve(completion: @escaping RetrieveCompletion)
+    func deleteCachedFeed() throws
+    func insert(_ feed: [LocalFeedImage], timestamp: Date) throws
+    func retrieve() throws -> CachedFeed?
 }
