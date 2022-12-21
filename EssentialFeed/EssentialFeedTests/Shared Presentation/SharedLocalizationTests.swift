b5c0d97152f5d3552399c464e0412d4b8dc8540b
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-21 09:54:56 -0300

    test: ensure localized strings have key and values for all supported lenguages

---
+++ EssentialFeed/EssentialFeedTests/Shared Presentation/SharedLocalizationTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+import EssentialFeed
+
+class SharedLocalizationTests: XCTestCase {
+    
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "Shared"
+        let bundle = Bundle(for: LoadResourcePresenter<Any, DummyView>.self)
+        
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+    
+    private class DummyView: ResourceView {
+        func display(_ viewModel: Any) {}
+    }
+    
+}
