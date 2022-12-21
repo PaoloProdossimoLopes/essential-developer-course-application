9e99f13c6cbf9f248feba2b9f7494c31d4fa1a2e
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-21 09:19:59 -0300

    feat: do the presenter feed works and localize

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImagePresenter.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public final class FeedImagePresenter {
+    public static func map(_ image: FeedImage) -> FeedImageViewModel {
+        FeedImageViewModel(
+            description: image.description,
+            location: image.location)
+    }
+}
---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -0,0 +1,8 @@
+public struct FeedImageViewModel {
+    public let description: String?
+    public let location: String?
+    
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
