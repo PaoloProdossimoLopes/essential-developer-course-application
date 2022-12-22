541dc382f803959dffa6c4ec4ffae3f0c0c85517
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-22 10:03:16 -0300

    test: ensure retrieve image delievers found data when there is a stored image data

--- EssentialFeed/EssentialFeedTests/LocalFeed/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/LocalFeed/CoreDataFeedImageDataStoreTests.swift
@@ -17,7 +17,17 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
         
         expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
     }
-   
+    
+    func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() {
+        let sut = makeSUT()
+        let storedData = anyData()
+        let matchingURL = URL(string: "http://a-url.com")!
+        
+        insert(storedData, for: matchingURL, into: sut)
+        
+        expect(sut, toCompleteRetrievalWith: found(storedData), for: matchingURL)
+    }
+    
     
     // - MARK: Helpers
     
