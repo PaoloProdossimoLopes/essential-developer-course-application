5283c273356b44cfecf315dc7e3f5be126b17655
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-22 09:19:38 -0300

    chore: creating test

--- EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -4,26 +4,9 @@ import XCTest
 import EssentialFeediOS
 @testable import EssentialFeed
 
-class ListSnapshotTests: XCTestCase {
+final class ListSnapshotTests: XCTestCase {
     
-    func test_emptyList() {
-        let sut = makeSUT()
-        
-        sut.display(emptyList())
-        
-        assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "EMPTY_LIST_light")
-        assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "EMPTY_LIST_dark")
-    }
     
-    func test_listWithErrorMessage() {
-        let sut = makeSUT()
-        
-        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
-        
-        assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
-        assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
-        assert(snapshot: sut.snapshot(for: .iPhone13(style: .light, contentSize: .extraExtraExtraLarge)), named: "LIST_WITH_ERROR_MESSAGE_light_extraExtraExtraLarge")
-    }
     
     // MARK: - Helpers
     
@@ -39,5 +22,5 @@ class ListSnapshotTests: XCTestCase {
     private func emptyList() -> [CellController] {
         return []
     }
-    
+
 }
