48bd23aa2b75ace0a23bf96b4c58a18e36cdcf73
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-22 09:20:17 -0300

    test: ensure list is empty on initialize en starts

--- EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -6,7 +6,16 @@ import EssentialFeediOS
 
 final class ListSnapshotTests: XCTestCase {
     
-    
+    func test_emptyList() {
+            let sut = makeSUT()
+            
+            sut.display(emptyList())
+            
+            assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "EMPTY_LIST_light")
+            assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "EMPTY_LIST_dark")
+        }
+        
+        
     
     // MARK: - Helpers
     
