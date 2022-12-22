3906e40e17c97ed8d4dc8f54dfa22bb47137fd25
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-22 09:35:53 -0300

    refactor: using helpers to improve

--- EssentialFeed/EssentialFeedTests/LocalFeed/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/LocalFeed/CoreDataFeedStoreTests.swift
@@ -3,4 +3,12 @@ import EssentialFeed
 
 final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
    
+    // - MARK: Helpers
+     
+     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
+         let storeURL = URL(fileURLWithPath: "/dev/null")
+         let sut = try! CoreDataFeedStore(storeURL: storeURL)
+         checkMemoryLeak(sut, file: file, line: line)
+         return sut
+     }
 }
