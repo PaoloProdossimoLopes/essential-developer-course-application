5e4973299532b534836af54132be7a78b0145936
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-22 10:13:50 -0300

    refactor: extract to helpers

--- EssentialFeed/EssentialFeedTests/LocalFeed/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/LocalFeed/ValidateFeedCacheUseCaseTests.swift
@@ -3,4 +3,30 @@ import EssentialFeed
 
 final class ValidateFeedCacheUseCaseTests: XCTestCase {
     
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        checkMemoryLeak(store, file: file, line: line)
+        checkMemoryLeak(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private func expect(_ sut: LocalFeedLoader, toCompleteWith expectedResult: Result<Void, Error>, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        action()
+
+        let receivedResult = Result { try sut.validateCache() }
+        
+        switch (receivedResult, expectedResult) {
+        case (.success, .success):
+            break
+            
+        case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+            XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            
+        default:
+            XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+        }
+    }
 }
