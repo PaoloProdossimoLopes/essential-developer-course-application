b01a663f1a459bf982367faf6f2cedd54c294d50
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-22 09:32:24 -0300

    test: add helpers

--- EssentialFeed/EssentialFeedTests/LocalFeed/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/LocalFeed/CacheFeedUseCaseTests.swift
@@ -2,5 +2,26 @@ import XCTest
 import EssentialFeed
 
 final class CacheFeedUseCaseTests: XCTestCase {
-  
+    
+    
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        checkMemoryLeak(store, file: file, line: line)
+        checkMemoryLeak(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private func expect(_ sut: LocalFeedLoader, toCompleteWithError expectedError: NSError?, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        action()
+        
+        do {
+            try sut.save(uniqueImageFeed().models)
+        } catch {
+            XCTAssertEqual(error as NSError?, expectedError, file: file, line: line)
+        }
+    }
 }
