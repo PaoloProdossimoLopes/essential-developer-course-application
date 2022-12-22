afdffef3eb9ab7409cd4cf5db7c9694b30486bdc
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-22 09:15:54 -0300

    test: craintg helpers for snapshot testing

--- EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
@@ -4,5 +4,42 @@ import EssentialFeediOS
 
 final class FeedSnapshotTests: XCTestCase {
     
+}
+
+private extension ListViewController {
+    func display(_ stubs: [ImageStub]) {
+        let cells: [CellController] = stubs.map { stub in
+            let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub, selection: {})
+            stub.controller = cellController
+            return CellController(id: UUID(), cellController)
+        }
+        
+        display(cells)
+    }
+}
+
+private class ImageStub: FeedImageCellControllerDelegate {
+    let viewModel: FeedImageViewModel
+    let image: UIImage?
+    weak var controller: FeedImageCellController?
     
+    init(description: String?, location: String?, image: UIImage?) {
+        self.viewModel = FeedImageViewModel(
+            description: description,
+            location: location)
+        self.image = image
+    }
+    
+    func didRequestImage() {
+        controller?.display(ResourceLoadingViewModel(isLoading: false))
+        
+        if let image = image {
+            controller?.display(image)
+            controller?.display(ResourceErrorViewModel(message: .none))
+        } else {
+            controller?.display(ResourceErrorViewModel(message: "any"))
+        }
+    }
+    
+    func didCancelImageRequest() {}
 }
