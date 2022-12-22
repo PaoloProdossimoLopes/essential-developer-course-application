5509249b83df75cf9b14b11373fc448545880df0
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-22 10:01:05 -0300

    test: ensure extract to helpers

--- EssentialFeed/EssentialFeedTests/LocalFeed/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/LocalFeed/CoreDataFeedImageDataStoreTests.swift
@@ -2,5 +2,47 @@ import XCTest
 import EssentialFeed
 
 final class CoreDataFeedImageDataStoreTests: XCTestCase {
-   
+    // - MARK: Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CoreDataFeedStore {
+        let storeURL = URL(fileURLWithPath: "/dev/null")
+        let sut = try! CoreDataFeedStore(storeURL: storeURL)
+        checkMemoryLeak(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func notFound() -> Result<Data?, Error> {
+        return .success(.none)
+    }
+    
+    private func found(_ data: Data) -> Result<Data?, Error> {
+        return .success(data)
+    }
+    
+    private func localImage(url: URL) -> LocalFeedImage {
+        return LocalFeedImage(id: UUID(), description: "any", location: "any", url: url)
+    }
+    
+    private func expect(_ sut: CoreDataFeedStore, toCompleteRetrievalWith expectedResult: Result<Data?, Error>, for url: URL,  file: StaticString = #filePath, line: UInt = #line) {
+        let receivedResult = Result { try sut.retrieve(dataForURL: url) }
+        
+        switch (receivedResult, expectedResult) {
+        case let (.success( receivedData), .success(expectedData)):
+            XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+            
+        default:
+            XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+        }
+    }
+    
+    private func insert(_ data: Data, for url: URL, into sut: CoreDataFeedStore, file: StaticString = #filePath, line: UInt = #line) {
+        do {
+            let image = localImage(url: url)
+            try sut.insert([image], timestamp: Date())
+            try sut.insert(data, for: url)
+        } catch {
+            XCTFail("Failed to insert \(data) with error \(error)", file: file, line: line)
+        }
+    }
+    
 }
