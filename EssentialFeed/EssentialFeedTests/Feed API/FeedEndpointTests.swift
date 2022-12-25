3006dc5007eaa455fd34a5d0c52f5c750cadfed2
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-25 10:07:06 -0300

    test: feed endpoint url

--- EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointTests.swift
@@ -3,5 +3,14 @@ import EssentialFeed
 
 class FeedEndpointTests: XCTestCase {
     
-    
+    func test_feed_endpointURL() {
+            let baseURL = URL(string: "http://base-url.com")!
+            
+            let received = FeedEndpoint.get().url(baseURL: baseURL)
+            
+            XCTAssertEqual(received.scheme, "http", "scheme")
+            XCTAssertEqual(received.host, "base-url.com", "host")
+            XCTAssertEqual(received.path, "/v1/feed", "path")
+            XCTAssertEqual(received.query, "limit=10", "query")
+        }
 }
