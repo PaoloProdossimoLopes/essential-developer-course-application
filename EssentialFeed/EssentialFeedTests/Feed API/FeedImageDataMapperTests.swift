236f2a874b90fcfe8ca0c1cede5a894d12588695
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-25 10:09:08 -0300

    test: ensure map throws error on non 200 HTTP response

--- EssentialFeed/EssentialFeedTests/Feed API/FeedImageDataMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedImageDataMapperTests.swift
@@ -3,5 +3,15 @@ import EssentialFeed
 
 class FeedImageDataMapperTests: XCTestCase {
     
-   
+    func test_map_throwsErrorOnNon200HTTPResponse() throws {
+            let samples = [199, 201, 300, 400, 500]
+            
+            try samples.forEach { code in
+                XCTAssertThrowsError(
+                    try FeedImageDataMapper.map(anyData(), from: HTTPURLResponse(statusCode: code))
+                )
+            }
+        }
+        
+        
 }
