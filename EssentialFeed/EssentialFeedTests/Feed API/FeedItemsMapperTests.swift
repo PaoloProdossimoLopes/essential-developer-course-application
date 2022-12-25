67516d4affd8b6d183e544fdcdd7916b2f7365bd
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-25 10:12:29 -0300

    chore: creating a helper

--- EssentialFeed/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
@@ -3,5 +3,19 @@ import EssentialFeed
 
 class FeedItemsMapperTests: XCTestCase {
     
-    
+    // MARK: - Helpers
+        
+        private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedImage, json: [String: Any]) {
+            let item = FeedImage(id: id, description: description, location: location, url: imageURL)
+            
+            let json = [
+                "id": id.uuidString,
+                "description": description,
+                "location": location,
+                "image": imageURL.absoluteString
+            ].compactMapValues { $0 }
+            
+            return (item, json)
+        }
+        
 }
