53788d81c38cbd04b2fa1993e30586859a170cb3
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-25 10:07:54 -0300

    test: ensure feed endpoint URL after given image

--- EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointTests.swift
@@ -13,4 +13,18 @@ class FeedEndpointTests: XCTestCase {
             XCTAssertEqual(received.path, "/v1/feed", "path")
             XCTAssertEqual(received.query, "limit=10", "query")
         }
+    
+    func test_feed_endpointURLAfterGivenImage() {
+            let image = uniqueImage()
+            let baseURL = URL(string: "http://base-url.com")!
+            
+            let received = FeedEndpoint.get(after: image).url(baseURL: baseURL)
+            
+            XCTAssertEqual(received.scheme, "http", "scheme")
+            XCTAssertEqual(received.host, "base-url.com", "host")
+            XCTAssertEqual(received.path, "/v1/feed", "path")
+            XCTAssertEqual(received.query?.contains("limit=10"), true, "limit query param")
+            XCTAssertEqual(received.query?.contains("after_id=\(image.id)"), true, "after_id query param")
+            
+        }
 }
