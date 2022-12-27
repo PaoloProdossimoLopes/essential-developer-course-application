7335d15d879369d58d7402418c8a996a769dfda5
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-27 09:44:26 -0300

    refactor: move image extensions its own file

---
+++ EssentialApp/EssentialApp/UIImage+Extensions.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension UIImage {
+    struct InvalidImageData: Error {}
+    
+    static func tryMake(data: Data) throws -> UIImage {
+        guard let image = UIImage(data: data) else {
+            throw InvalidImageData()
+        }
+        return image
+    }
+}
