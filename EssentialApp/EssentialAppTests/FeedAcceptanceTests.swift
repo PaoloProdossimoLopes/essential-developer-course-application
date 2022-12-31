85bdc2d79391117bce238927558b75b1b8ca66d4
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-30 22:01:44 -0300

    refactor: extract a data created

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -109,4 +109,32 @@ class FeedAcceptanceTests: XCTestCase {
         return (makeData(for: url), response)
     }
     
+    private func makeData(for url: URL) -> Data {
+        switch url.path {
+        case "/image-0": return makeImageData0()
+        case "/image-1": return makeImageData1()
+        case "/image-2": return makeImageData2()
+            
+        case "/essential-feed/v1/feed" where url.query?.contains("after_id") == false:
+            return makeFirstFeedPageData()
+            
+        case "/essential-feed/v1/feed" where url.query?.contains("after_id=A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A") == true:
+            return makeSecondFeedPageData()
+            
+        case "/essential-feed/v1/feed" where url.query?.contains("after_id=166FCDD7-C9F4-420A-B2D6-CE2EAFA3D82F") == true:
+            return makeLastEmptyFeedPageData()
+            
+        case "/essential-feed/v1/image/2AB2AE66-A4B7-4A16-B374-51BBAC8DB086/comments":
+            return makeCommentsData()
+            
+        default:
+            return Data()
+        }
+    }
+    
+    private func makeImageData0() -> Data { UIImage.make(withColor: .red).pngData()! }
+    private func makeImageData1() -> Data { UIImage.make(withColor: .green).pngData()! }
+    private func makeImageData2() -> Data { UIImage.make(withColor: .blue).pngData()! }
+    
+    
 }
