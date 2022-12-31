b4a5540fe4a040b6b58880f50fa82b8a1a011a1d
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-30 21:48:41 -0300

    test: ensure load dispatches from bg thread

--- EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -92,6 +92,18 @@ class CommentsUIIntegrationTests: XCTestCase {
         assertThat(sut, isRendering: [comment])
     }
     
+    func test_loadCommentsCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        
+        let exp = expectation(description: "Wait for background queue")
+        DispatchQueue.global().async {
+            loader.completeCommentsLoading(at: 0)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     
     // MARK: - Helpers
     
