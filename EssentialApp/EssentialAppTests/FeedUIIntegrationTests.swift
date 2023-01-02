dc26c7f3b3056ac117bcd5e2bad41f24ec1f9d62
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-02 09:36:36 -0300

    chore: creating a file for feed integration tests with helpers

---
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -0,0 +1,36 @@
+import XCTest
+import UIKit
+import EssentialApp
+import EssentialFeed
+import EssentialFeediOS
+
+class FeedUIIntegrationTests: XCTestCase {
+    
+    
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        selection: @escaping (FeedImage) -> Void = { _ in },
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (sut: ListViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = FeedUIComposer.feedComposedWith(
+            feedLoader: loader.loadPublisher,
+            imageLoader: loader.loadImageDataPublisher,
+            selection: selection
+        )
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, loader)
+    }
+    
+    private func makeImage(description: String? = nil, location: String? = nil, url: URL = URL(string: "http://any-url.com")!) -> FeedImage {
+        return FeedImage(id: UUID(), description: description, location: location, url: url)
+    }
+    
+    private func anyImageData() -> Data {
+        return UIImage.make(withColor: .red).pngData()!
+    }
+}
