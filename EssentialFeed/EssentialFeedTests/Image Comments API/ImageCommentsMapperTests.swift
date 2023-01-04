736a79859f8e6fc06b542c6126d7512917119243
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-04 08:13:48 -0300

    test: map throwes error on 2xx HTTP Resposne with invalid json

--- EssentialFeed/EssentialFeedTests/Image Comments API/ImageCommentsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments API/ImageCommentsMapperTests.swift
@@ -14,6 +14,17 @@ class ImageCommentsMapperTests: XCTestCase {
         }
     }
     
+    func test_map_throwsErrorOn2xxHTTPResponseWithInvalidJSON() throws {
+        let invalidJSON = Data("invalid json".utf8)
+        let samples = [200, 201, 250, 280, 299]
+        
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try ImageCommentsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: code))
+            )
+        }
+    }
+    
     
     private func makeItem(id: UUID, message: String, createdAt: (date: Date, iso8601String: String), username: String) -> (model: ImageComment, json: [String: Any]) {
         let item = ImageComment(id: id, message: message, createdAt: createdAt.date, username: username)
