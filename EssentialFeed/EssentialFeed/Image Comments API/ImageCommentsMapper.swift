cc9392f25647c3b541e9c83868593cc22366ab65
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-04 07:57:43 -0300

    feat: creating a sub-struct to decode response

--- EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
@@ -1,5 +1,23 @@
 import Foundation
 
 public final class ImageCommentsMapper {
+    private struct Root: Decodable {
+        private let items: [Item]
+        
+        private struct Item: Decodable {
+            let id: UUID
+            let message: String
+            let created_at: Date
+            let author: Author
+        }
+        
+        private struct Author: Decodable {
+            let username: String
+        }
+        
+        var comments: [ImageComment] {
+            items.map { ImageComment(id: $0.id, message: $0.message, createdAt: $0.created_at, username: $0.author.username) }
+        }
+    }
     
 }
