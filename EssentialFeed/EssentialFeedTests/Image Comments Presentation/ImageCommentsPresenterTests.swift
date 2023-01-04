a6d6a8603a8fdcb4143cea4a3694ded8986cacbb
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-04 08:44:02 -0300

    feat: creating a helper to get text localized

--- EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -4,4 +4,14 @@ import EssentialFeed
 class ImageCommentsPresenterTests: XCTestCase {
     
     
+    private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+    
 }
