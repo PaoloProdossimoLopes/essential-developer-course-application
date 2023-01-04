b1d314930f983c0263a41a9b345e3c67dc9ff9d2
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-04 08:12:29 -0300

    chore: creating a helper to create a model to help in test

--- EssentialFeed/EssentialFeedTests/Image Comments API/ImageCommentsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments API/ImageCommentsMapperTests.swift
@@ -2,5 +2,21 @@ import XCTest
 import EssentialFeed
 
 class ImageCommentsMapperTests: XCTestCase {
-   
+    
+    
+    private func makeItem(id: UUID, message: String, createdAt: (date: Date, iso8601String: String), username: String) -> (model: ImageComment, json: [String: Any]) {
+        let item = ImageComment(id: id, message: message, createdAt: createdAt.date, username: username)
+        
+        let json: [String: Any] = [
+            "id": id.uuidString,
+            "message": message,
+            "created_at": createdAt.iso8601String,
+            "author": [
+                "username": username
+            ]
+        ]
+        
+        return (item, json)
+    }
+    
 }
