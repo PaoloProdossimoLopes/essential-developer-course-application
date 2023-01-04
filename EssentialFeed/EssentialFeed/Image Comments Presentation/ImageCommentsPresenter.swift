b9573cb4c972f927b57e76a0d9c58f85aa635467
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-04 08:17:47 -0300

    feat: creating a `ImageCommentViewModel` to pass to view

--- EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -1,2 +1,14 @@
 import Foundation
 
+
+public struct ImageCommentViewModel: Hashable {
+    public let message: String
+    public let date: String
+    public let username: String
+    
+    public init(message: String, date: String, username: String) {
+        self.message = message
+        self.date = date
+        self.username = username
+    }
+}
