b557ecaf8dab5bea853e26d1d2df4e978632a353
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-04 07:58:09 -0300

    feat: handling with decodification

--- EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
@@ -20,4 +20,22 @@ public final class ImageCommentsMapper {
         }
     }
     
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+        guard isOK(response), let root = try? decoder.decode(Root.self, from: data) else {
+            throw Error.invalidData
+        }
+        
+        return root.comments
+    }
+    
+    private static func isOK(_ response: HTTPURLResponse) -> Bool {
+        (200...299).contains(response.statusCode)
+    }
 }
