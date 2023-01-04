e85c15d5790a7cf0eef54b331547fa89e9a26a40
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-04 08:18:50 -0300

    feat: creating a presenter with title and map method

--- EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -16,3 +16,31 @@ public struct ImageCommentViewModel: Hashable {
 public struct ImageCommentsViewModel {
     public let comments: [ImageCommentViewModel]
 }
+
+public final class ImageCommentsPresenter {
+    public static var title: String {
+        NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE",
+                          tableName: "ImageComments",
+                          bundle: Bundle(for: Self.self),
+                          comment: "Title for the image comments view")
+    }
+    
+    public static func map(
+        _ comments: [ImageComment],
+        currentDate: Date = Date(),
+        calendar: Calendar = .current,
+        locale: Locale = .current
+    ) -> ImageCommentsViewModel {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.calendar = calendar
+        formatter.locale = locale
+        
+        return ImageCommentsViewModel(comments: comments.map { comment in
+            ImageCommentViewModel(
+                message: comment.message,
+                date: formatter.localizedString(for: comment.createdAt, relativeTo: currentDate),
+                username: comment.username)
+        })
+    }
+}
+
