e05bfcce2939e176095ad4c3a8af901447ca93b1
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-04 08:41:45 -0300

    test: localized strings vake key and valus for all supported localizations

--- EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsLocalizationTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsLocalizationTests.swift
@@ -3,5 +3,11 @@ import EssentialFeed
 
 class ImageCommentsLocalizationTests: XCTestCase {
     
-
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+    
 }
