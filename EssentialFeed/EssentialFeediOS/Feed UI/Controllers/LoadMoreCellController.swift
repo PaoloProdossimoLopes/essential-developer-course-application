6b24bd2432c4976dd3ac1f75469cd646ece2cf29
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-08 22:19:56 -0300

    feat: load more when scrolling

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -0,0 +1,109 @@
+import UIKit
+import EssentialFeed
+
+public class LoadMoreCell: UITableViewCell {
+    
+    private lazy var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .medium)
+        contentView.addSubview(spinner)
+        
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            contentView.heightAnchor.constraint(greaterThanOrEqualToConstant: 40)
+        ])
+        
+        return spinner
+    }()
+    
+    private lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .tertiaryLabel
+        label.font = .preferredFont(forTextStyle: .footnote)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        contentView.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            contentView.trailingAnchor.constraint(equalTo: label.trailingAnchor, constant: 8),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            contentView.bottomAnchor.constraint(equalTo: label.bottomAnchor, constant: 8),
+        ])
+        
+        return label
+    }()
+    
+    public var isLoading: Bool {
+        get { spinner.isAnimating }
+        set {
+            if newValue {
+                spinner.startAnimating()
+            } else {
+                spinner.stopAnimating()
+            }
+        }
+    }
+    
+    public var message: String? {
+        get { messageLabel.text }
+        set { messageLabel.text = newValue }
+    }
+    
+}
+
+public class LoadMoreCellController: NSObject, UITableViewDataSource, UITableViewDelegate {
+    private let cell = LoadMoreCell()
+    private let callback: () -> Void
+    private var offsetObserver: NSKeyValueObservation?
+    
+    public init(callback: @escaping () -> Void) {
+        self.callback = callback
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, willDisplay: UITableViewCell, forRowAt indexPath: IndexPath) {
+        reloadIfNeeded()
+        
+        offsetObserver = tableView.observe(\.contentOffset, options: .new) { [weak self] (tableView, _) in
+            guard tableView.isDragging else { return }
+            
+            self?.reloadIfNeeded()
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        offsetObserver = nil
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        reloadIfNeeded()
+    }
+    
+    private func reloadIfNeeded() {
+        guard !cell.isLoading else { return }
+        
+        callback()
+    }
+}
+
+extension LoadMoreCellController: ResourceLoadingView, ResourceErrorView {
+    public func display(_ viewModel: ResourceErrorViewModel) {
+        cell.message = viewModel.message
+    }
+    
+    public func display(_ viewModel: ResourceLoadingViewModel) {
+        cell.isLoading = viewModel.isLoading
+    }
+}
