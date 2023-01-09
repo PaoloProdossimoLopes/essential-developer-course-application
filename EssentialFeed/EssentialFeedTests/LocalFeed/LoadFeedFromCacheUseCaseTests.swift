b0f865a653c95081f6674e945520fc271ae0c801
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-09 09:18:58 -0300

    refactor: fix indentation

--- EssentialFeed/EssentialFeedTests/LocalFeed/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/LocalFeed/LoadFeedFromCacheUseCaseTests.swift
@@ -3,10 +3,10 @@ import EssentialFeed
 
 final class LoadFeedFromCacheUseCaseTests: XCTestCase {
     func test_init_doesNotMessageStoreUponCreation() {
-            let (_, store) = makeSUT()
-            
-            XCTAssertEqual(store.receivedMessages, [])
-        }
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
     
     func test_load_requestsCacheRetrieval() {
         let (sut, store) = makeSUT()
@@ -119,33 +119,34 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
+}
+
+// MARK: - Helpers
+private extension LoadFeedFromCacheUseCaseTests {
     
-
-    // MARK: - Helpers
-       
-       private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
-           let store = FeedStoreSpy()
-           let sut = LocalFeedLoader(store: store, currentDate: currentDate)
-           checkMemoryLeak(store, file: file, line: line)
-           checkMemoryLeak(sut, file: file, line: line)
-           return (sut, store)
-       }
-       
-       private func expect(_ sut: LocalFeedLoader, toCompleteWith expectedResult: Result<[FeedImage], Error>, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
-           action()
-
-           let receivedResult = Result { try sut.load() }
-           
-           switch (receivedResult, expectedResult) {
-           case let (.success(receivedImages), .success(expectedImages)):
-               XCTAssertEqual(receivedImages, expectedImages, file: file, line: line)
-               
-           case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
-               XCTAssertEqual(receivedError, expectedError, file: file, line: line)
-               
-           default:
-               XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-           }
-       }
-       
+    func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        checkMemoryLeak(store, file: file, line: line)
+        checkMemoryLeak(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    func expect(_ sut: LocalFeedLoader, toCompleteWith expectedResult: Result<[FeedImage], Error>, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        action()
+        
+        let receivedResult = Result { try sut.load() }
+        
+        switch (receivedResult, expectedResult) {
+        case let (.success(receivedImages), .success(expectedImages)):
+            XCTAssertEqual(receivedImages, expectedImages, file: file, line: line)
+            
+        case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+            XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            
+        default:
+            XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+        }
+    }
+    
 }
