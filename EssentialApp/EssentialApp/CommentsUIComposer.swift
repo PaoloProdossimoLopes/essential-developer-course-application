f0cd19a6f3ad26ca9bb3d9ca564d16200ebfe8fc
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-01-09 08:46:11 -0300

    refactor: move `CommentsViewAdapter` its own file

--- EssentialApp/EssentialApp/CommentsUIComposer.swift
+++ EssentialApp/EssentialApp/CommentsUIComposer.swift
@@ -32,17 +32,3 @@ public enum CommentsUIComposer {
         return controller
     }
 }
-
-final class CommentsViewAdapter: ResourceView {
-    private weak var controller: ListViewController?
-    
-    init(controller: ListViewController) {
-        self.controller = controller
-    }
-    
-    func display(_ viewModel: ImageCommentsViewModel) {
-        controller?.display(viewModel.comments.map { viewModel in
-            CellController(id: viewModel, ImageCommentCellController(model: viewModel))
-        })
-    }
-}
